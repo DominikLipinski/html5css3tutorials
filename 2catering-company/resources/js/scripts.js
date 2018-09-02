afb2077fd0dd5ba0996dc099fe393dde64baa3d1
DominikLipinski <maboonga@gmail.com>
2018-09-02 10:08:46 +0200

    animations on scroll

--- 2catering-company/resources/js/scripts.js
+++ 2catering-company/resources/js/scripts.js
@@ -27,7 +27,6 @@ $('a[href*="#"]')
   .not('[href="#"]')
   .not('[href="#0"]')
   .click(function(event) {
-    // On-page links
     if (
       location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
       && 
@@ -53,5 +52,30 @@ $('a[href*="#"]')
     }
   });
 
+/* animation on scroll */
+
+$('.js--wp-1').waypoint(function(direction) {
+  $('.js--wp-1').addClass('animated fadeIn');
+}, {
+  offset: '50%'
+});
+
+$('.js--wp-2').waypoint(function(direction) {
+  $('.js--wp-2').addClass('animated fadeInUp');
+}, {
+  offset: '50%'
+});
+
+$('.js--wp-3').waypoint(function(direction) {
+  $('.js--wp-3').addClass('animated fadeIn');
+}, {
+  offset: '50%'
+});
+
+$('.js--wp-4').waypoint(function(direction) {
+  $('.js--wp-4').addClass('animated pulse');
+}, {
+  offset: '50%'
+});
 
 });
